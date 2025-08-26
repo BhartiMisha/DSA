ed0ca47910c84846c43d7b42066480e2cef268a5
mbharti <mbhart@commvault.com>
2025-08-27 00:07:55 +0530

    Added more approach to Valid anagram problem

--- WEEK-01-Arrays-Hashings/DAY-02-Valid_Anagram-Valid_Anagram/01_ValidAnagram.cpp
+++ WEEK-01-Arrays-Hashings/DAY-02-Valid_Anagram-Valid_Anagram/01_ValidAnagram.cpp
@@ -32,7 +32,20 @@ Follow up: What if the inputs contain Unicode characters? How would you adapt yo
 /*
 APPROACHES:
 
-1. Using unordered_map
+1. Brute-force
+    If the size of the string does not match, return false.
+    sort string 1
+    sort string 2
+    if string 1 == string true then return true
+    else return false
+
+    Time Complexity: O(n log n) [sorting]
+    Space Complexity: O(1)
+
+
+
+
+2. Using unordered_map
     if the size of the strings does not match, return false.
     Create a map and store the frequency of each char of first string in the map
     Iterate over the other string and keep on decreasing the frequency for each element
@@ -42,25 +55,61 @@ APPROACHES:
 
     Time complexity: O(n)
     Space complexity: O(n)
+
+
+
+3. Using frequency list (only in case if all the elements in the string is either upper case or lower case)
+    Create a frequency list with size 26 (total alphabets) with all value as 0
+    iterate over string 1 and increase the frequency of each char
+    iterate over string 2 and decrease the frequency of each char
+    iterate over the freq list and check the value
+    if any value is not zero then the strings are not valid anagram.
+    else they are valid anagrams.
+
+    Time complexity: O(n)
+    Space Complexity: O(n)
+    
+     
 */
 
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <algorithm>   // needed for sort()
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        // Approach 1: Unordered map
-        unordered_map<char, int> m;
-        for(char c: s) m[c]++;
-        for(char c: t) m[c]--;
+        // Approach 1: brute-force
+        // if(! s.size() == t.size()) 
+        //     return false;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
 
-        for(auto& p: m){
-            if(p.second != 0)
-                return false;
+
+        // Approach 2: Unordered map
+        // unordered_map<char, int> m;
+        // for(char c: s) m[c]++;
+        // for(char c: t) m[c]--;
+
+        // for(auto& p: m){
+        //     if(p.second != 0)
+        //         return false;
+        // }
+        // return true;
+
+
+        // Approach 3: Using frequency list 
+        int freq[26] = {0};
+
+        for (char c : s) freq[c - 'a']++;
+        for (char c : t) freq[c - 'a']--;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) return false;
         }
         return true;
     }
