ac9fead6321221b93a099182bc170fdded40e2f0
Misha Bharti <shivaniroyshr25@gmail.com>
2025-09-05 00:26:38 +0530

    Day 10, Question 01 - Better solution

--- WEEK-02-Two_Pointers-Sliding_Window/DAY-03-Trapping_Rain_Water-Remove_Duplicates_from_Sorted_Array/01-TrappingRainWater.cpp
+++ WEEK-02-Two_Pointers-Sliding_Window/DAY-03-Trapping_Rain_Water-Remove_Duplicates_from_Sorted_Array/01-TrappingRainWater.cpp
@@ -24,7 +24,7 @@ n == height.length
 APPROACHES:
 
 1. Brute-Force (TIME LIMIT EXCEEDED)
-    IDEA: For any given bar at index i, the amount of water it can trap depends on the tallest bar to its left and the tallest bar to its right.
+    IDEA: For any given bar at index i, the amount of water it can trap (in that exact position) depends on the tallest bar to its left and the tallest bar to its right.
     The water above height[i] is determined by the smaller of these two walls (min(leftMax, rightMax)), because water will spill over the shorter side.
     Then subtract the height of the current bar (height[i]) to get the trapped water at that position.
     If the result is negative, we take 0 (because water can’t go below ground level).
@@ -40,6 +40,30 @@ APPROACHES:
 
     Time Complexity: O(n^2)
     Space Complexity: O(1)
+
+
+
+
+2. Storing the max heights in array
+    IDEA: In one forward traversal, compute the maximum height to the left of each element (leftMax).
+    In one backward traversal, compute the maximum height to the right of each element (rightMax).
+    During the final traversal, use these precomputed arrays to quickly find trapped water at each index.
+    Formula: 
+        trapped_water[i] = max(0, min(leftMax[i], rightMax[i]) - height[i])
+
+    LOGIC:
+    Create two arrays to store the maximum height on the left and right of the current element
+    Start a forward traversal and for every element store the maximum of prev and current (leftMax). For 0th element store zero since there's no element on the left of it.
+    Start a backward traversal and for every element store the max of prevRight and current (rightMax). For (n-1)th element store zero since there's no element on the right of it.
+    Now in the final forward traversal, keep on adding to the result with the formula mentioned in the IDEA part.
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+
+
+
+3. Two Pointer
 */
 
 
@@ -49,17 +73,48 @@ using namespace std;
 class Solution {
     public:
         int trap(vector<int>& height) {
+            // // APPROACH 1: BRUTE-FORCE
+            // int res = 0;
+            // for(int i = 0; i < height.size(); i++) {
+            //     int leftMax = 0, rightMax = 0;
+            //     for(int j = 0; j <= i; j++) {
+            //         leftMax = max(leftMax, height[j]);
+            //     }
+            //     for(int j = i + 1; j < height.size(); j++) {
+            //         rightMax = max(rightMax, height[j]);
+            //     }
+            //     res += max(0, min(leftMax, rightMax) - height[i]);
+            // } 
+            // return res;
+
+
+
+
+            // APPROACH 2: STORING THE MAX HEIGHTS IN ARRAY
+            int n = height.size();
+            vector<int> left(n), right(n);
+            int max_ = 0;
+            for(int i = 0; i < n; i++) {
+                if(i == 0) {
+                    max_ = 0;
+                } else {
+                    max_ = max(max_, height[i-1]);
+                }
+                left[i] = max_;
+            }
+            max_ = 0;
+            for(int i = n - 1; i >= 0; i--) {
+                if(i == n - 1) {
+                    max_ = 0;
+                } else {
+                    max_ = max(max_, height[i+1]);
+                }
+                right[i] = max_;
+            }
             int res = 0;
-            for(int i = 0; i < height.size(); i++) {
-                int leftMax = 0, rightMax = 0;
-                for(int j = 0; j <= i; j++) {
-                    leftMax = max(leftMax, height[j]);
-                }
-                for(int j = i + 1; j < height.size(); j++) {
-                    rightMax = max(rightMax, height[j]);
-                }
-                res += max(0, min(leftMax, rightMax) - height[i]);
-            } 
+            for(int i = 0; i < n; i++) {
+                res += max(0, min(left[i], right[i]) - height[i]);
+            }
             return res;
         }
     };
